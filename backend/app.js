7da7f79666880e29cec54ea8c74ab5b4f172433f
soukainachekkouri <58041264+soukainachekkouri@users.noreply.github.com>
2022-08-17 18:49:13 +0200

    maj

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
+const path = require('path');
 
 mongoose.connect("mongodb+srv://root:root@cluster0.2ex4l.mongodb.net/?retryWrites=true&w=majority", {
         useNewUrlParser: true,
@@ -26,7 +27,7 @@ app.use((req, res, next) => {
 
 
 app.use('/api/auth', userRoutes);
-app.use("/api/sauces", sauceRoutes);
-
+app.use('/api/sauces', sauceRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,12 +1,12 @@
 const Sauce = require('../models/sauce');
 
 exports.createSauce = (req, res, next) => {
-    const sauces = JSON.parse(req.body.sauce)
+    const sauces = JSON.parse(req.body.sauce);
+    delete sauces._id;
     const newSauce = new Sauce({
         ...sauces,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
         likes: 0,
-        userId: "62f136ee90b72c48bd3dff29",
         dislikes: 0,
         usersLiked: [],
         usersDisliked: []
@@ -14,4 +14,44 @@ exports.createSauce = (req, res, next) => {
     newSauce.save()
         .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
         .catch((error) => res.status(400).json({ error }))
+}
+
+exports.findSauces = (req, res, next) => {
+    Sauce.save()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }))
+}
+
+exports.findOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json({ sauce }))
+        .catch(error => res.status(400).json({ error }));
+}
+
+exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    } : {...req.body };
+
+    delete sauceObject.userId;
+    Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => {
+            if (sauce.userId != req.auth.userId) {
+                res.status(401).json({ message: 'Non-autorisé' })
+            } else {
+                Sauce.updateOne({ _id: req.params.id }, {...sauceObject, _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Objet modifé' }))
+                    .catch(error => res.status(401).json({ error }))
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        })
+};
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'objet supprimé!' }))
+        .catch(error => res.status(400).json({ error }));
 }
