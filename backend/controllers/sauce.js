a7750286f9da9257c03bb8a6981975ac50554344
soukainachekkouri <58041264+soukainachekkouri@users.noreply.github.com>
2022-08-17 21:06:48 +0200

    maj

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -17,15 +17,18 @@ exports.createSauce = (req, res, next) => {
 }
 
 exports.findSauces = (req, res, next) => {
-    Sauce.save()
+    Sauce.find()
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({ error }))
 }
 
 exports.findOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
-        .then(sauce => res.status(200).json({ sauce }))
-        .catch(error => res.status(400).json({ error }));
+        .then(function(sauce) {
+            console.log(sauce);
+            res.status(200).json({ sauce })
+        })
+        .catch(error => res.status(400).json(error));
 }
 
 exports.modifySauce = (req, res, next) => {
